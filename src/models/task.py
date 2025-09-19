d0fa4b135b01dc748db89aaf83f58e0d3a93bca1
Princewill Okereke <vincentprincewill44@gmail.com>
2025-09-19 15:16:36 +0100

    update on ui and backend features

--- src/models/task.py
+++ src/models/task.py
@@ -1,18 +1,25 @@
+from datetime import datetime
+
 class Task:
-   def __init__(self, name, priority="medium", deadline=None, completed=False):
-      self.name = name
-      self.priority = priority
-      self.deadline = None
-      self.completed = completed
+   def __init__(self, title:str, priority:str="medium", deadline:datetime=None, completed:bool=False):
+      
+      self.title:str = title
+      self.priority:str = priority
+      self.deadline: datetime | str = None
+      self.completed:bool = completed
+
       if deadline:
          self.set_deadline(deadline)
 
    def __repr__(self):
-      return f"Task(name={self.name}, priority={self.priority})"
+      return f"Task(name={self.title}, priority={self.priority})"
    
-   def set_deadline(self, deadline):
+   def is_overdue(self):
+      return not self.completed and datetime.now() > self.deadline
+
+
+   def set_deadline(self, deadline: datetime | str):
       if isinstance(deadline, str):
-         from datetime import datetime
          try:
             self.deadline = datetime.strptime(deadline, "%Y-%m-%d %H:%M")
          except ValueError:
@@ -25,20 +32,19 @@ class Task:
 
    def to_dict(self):
       return {
-         "name": self.name,
+         "title": self.title,
          "priority": self.priority,
          "deadline": self.deadline.isoformat() if self.deadline else None,
          "completed": self.completed
       }
    
    @classmethod
-   def from_dict(cls, data):
+   def from_dict(cls, data: dict):
       deadline = data.get("deadline")
       if deadline:
-         from datetime import datetime
          deadline = datetime.fromisoformat(deadline)
       return cls(
-         name=data["name"],
+         title=data["title"],
          priority=data["priority"],
          deadline=deadline,
          completed=data.get("completed", False)
